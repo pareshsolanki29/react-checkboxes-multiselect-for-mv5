c979edd1d5f21343207caae1d88f230ad1ca8ea1
pareshsolanki29 <bandralites@gmail.com>
2022-05-02 23:15:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-xfcpdn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-xfcpdn)
---
+++ data.tsx
@@ -0,0 +1,10 @@
+export const top100 = [
+  { name1: 'The Shawshank Redemption', year: 1994 },
+  { name1: 'The Godfather', year: 1972 },
+  { name1: 'The Godfather: Part II', year: 1974 },
+  { name1: 'The Dark Knight', year: 2008 },
+  { name1: '12 Angry Men', year: 1957 },
+  { name1: "Schindler's List", year: 1993 },
+  { name1: 'Pulp Fiction', year: 1994 },
+];
+export const name = 'name';
